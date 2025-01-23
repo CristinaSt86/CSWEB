55fdfdb02c68f018b95e915280722f17dfc2de77
Cristina Stoian <cristina.stoian@yahoo.com>
2025-01-23 13:14:58 +0100

    Decrease the slide animation of the banner.

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,8 +22,8 @@ module.exports = {
       },
 
       animation: {
-        slideIn: "slideIn 3s ease-out forwards",
-        slideOut: "slideOut 3s ease-in forwards",
+        slideIn: "slideIn 2s ease-out forwards",
+        slideOut: "slideOut 2s ease-in forwards",
         fadeInUp: "fadeInUp 1.5s cubic-bezier(0.5, 1, 0.89, 1) forwards", 
       },
       keyframes: {
