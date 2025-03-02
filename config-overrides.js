c1b76153df75cb67d143e4cbaa9c27f7c8717e63
Cristina Stoian <cristina.stoian@yahoo.com>
2025-03-02 23:32:24 +0100

    Test

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,32 @@
-const { override, addWebpackPlugin } = require('customize-cra');
+const { override } = require('customize-cra');
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = override(
-  addWebpackPlugin(
-    new TerserPlugin({
-      terserOptions: {
-        compress: {
-          drop_console: true, // Remove console logs
-        },
-      },
-    })
-  )
+  (config) => {
+    if (config.optimization && config.optimization.minimizer) {
+      // Modify the existing TerserPlugin options
+      config.optimization.minimizer = config.optimization.minimizer.map(
+        (plugin) => {
+          if (plugin instanceof TerserPlugin) {
+            return new TerserPlugin({
+              terserOptions: {
+                compress: {
+                  drop_console: true, // Remove console logs
+                  drop_debugger: true, // Remove debugger statements
+                  unused: true, // Remove unused variables
+                  dead_code: true, // Remove dead code
+                },
+                output: {
+                  comments: false, // Remove comments
+                },
+              },
+              extractComments: false, // Prevent generating LICENSE.txt
+            });
+          }
+          return plugin;
+        }
+      );
+    }
+    return config;
+  }
 );
