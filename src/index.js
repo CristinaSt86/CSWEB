f78339ba8f0e9c35897d5fd2f4e8a47917f88fdb
Cristina Stoian <cristina.stoian@yahoo.com>
2025-03-03 11:18:46 +0100

    Fix Translation bug

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './i18n';
+import './i18n';  // Asigură-te că fișierul i18n este importat corect
 import LanguageSwitcher from './components/LanguageSwitcher';
+import { I18nextProvider } from 'react-i18next';  // Importă I18nextProvider
+import i18n from './i18n';  // Asigură-te că importi instanța i18n
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <LanguageSwitcher />
+    <I18nextProvider i18n={i18n}>  {/* Învelește aplicația cu I18nextProvider */}
+      <App />
+      <LanguageSwitcher />
+    </I18nextProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
